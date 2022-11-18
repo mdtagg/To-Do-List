76ac3b1172ac181ac4523b5985870c738263533c
Mike Tagg <mdtagg91@gmail.com>
2022-11-18 12:31:56 -0800

    fixing modal pop up bugs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import './styles.css'
+import taskPopUp from './taskPopUp'
 
 const addTaskButton = document.getElementById('add-task-button')
-const body = document.getElementById('body')
-const container = document.getElementById('container')
-
-const taskPopUp = () => {
-    const PopUp = document.createElement('div')
-    PopUp.classList.add('taskModal')
-    container.style.zIndex = '2'
-    body.appendChild(PopUp)
-}
 
 addTaskButton.addEventListener('click', taskPopUp)
+
+
+
+
+
+
+
---
+++ src/taskPopUp.js
@@ -0,0 +1,10 @@
+
+
+const taskPopUp = () => {
+    const body = document.getElementById('body')
+    const PopUp = document.createElement('div')
+    PopUp.classList.add('taskModal')
+    body.appendChild(PopUp)
+    }
+
+export default taskPopUp
