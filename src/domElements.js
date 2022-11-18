fceecca312cd1253911c55f17edee19570cf3e80
Mike Tagg <mdtagg91@gmail.com>
2022-11-17 16:08:28 -0800

    starting logic for adding tasks

---
+++ src/domElements.js
@@ -0,0 +1,14 @@
+
+const domElements = () => {
+    
+    const createTaskButton = () => {
+
+        const taskButton = document.getElementById('add-task-button')
+        taskButton.addEventListener('click', )
+        return taskButton
+    } 
+
+    return createTaskButton
+}
+
+export default domElements
---
+++ src/new_task.js
@@ -0,0 +1,12 @@
+import domElements from './domElements'
+
+const Task = () => {
+    
+console.log(domElements)
+
+return Task
+}
+
+export default Task
+
+
