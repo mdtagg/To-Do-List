5d180ac1bf0a681ec1752164972ed59d01fbbab8
Mike Tagg <mdtagg91@gmail.com>
2022-11-22 18:04:54 -0800

    Task details object reworked, redundant code removed

--- src/addNew.js
+++ src/addNew.js
@@ -1,39 +1,18 @@
 
-// const elementsObject = () => {
-
-    // const taskModal = document.getElementById('taskModal')
-    // taskModal.style.display = 'flex'
-    // elements.form.classList.add('taskForm')
-
-//     const form = elementCreatorTestTest('div','form')
-//     const taskHeaderContainer = elementCreatorTestTest('div','taskHeaderContainer')
-//     const taskHeader = elementCreatorTestTest('div', 'taskHeader','Add new...')
-//     const closeTask = elementCreatorTestTest('div','closeTask','X')
-//     const taskContainer = elementCreatorTestTest('div','taskContainer')
-//     const taskSideBar = elementCreatorTestTest('div','taskSidebar')
-//     const taskToDoTab = elementCreatorTestTest('div', 'toDoTab', 'To Do')
-//     const taskDetails = elementCreatorTestTest('div', 'taskDetails')
-
-
-
-//     return { form,taskHeaderContainer,taskHeader,closeTask,taskContainer,
-//         taskSideBar,taskToDoTab,taskDetails }
-// }
-
 const taskPopUp = () => {
 
     const taskModal = document.getElementById('taskModal')
     taskModal.style.display = 'flex'
-    const form = elementCreatorTestTest('div','form')
+    const form = elementCreator('div','form')
     form.classList.add('taskForm')
-    const taskHeaderContainer = elementCreatorTestTest('div','taskHeaderContainer')
-    const taskHeader = elementCreatorTestTest('div', 'taskHeader','Add new...')
-    const closeTask = elementCreatorTestTest('div','closeTask','X')
+    const taskHeaderContainer = elementCreator('div','taskHeaderContainer')
+    const taskHeader = elementCreator('div', 'taskHeader','Add new...')
+    const closeTask = elementCreator('div','closeTask','X')
     closeTask.addEventListener('click',closeTask)
-    const taskContainer = elementCreatorTestTest('div','taskContainer')
-    const taskSideBar = elementCreatorTestTest('div','taskSidebar')
-    const taskToDoTab = elementCreatorTestTest('div', 'toDoTab', 'To Do')
-    const taskDetails = elementCreatorTestTest('div', 'taskDetails')
+    const taskContainer = elementCreator('div','taskContainer')
+    const taskSideBar = elementCreator('div','taskSidebar')
+    const taskToDoTab = elementCreator('div', 'toDoTab', 'To Do')
+    const taskDetails = elementCreator('div', 'taskDetails')
 
     taskModal.appendChild(form)
     form.appendChild(taskHeaderContainer)
@@ -56,53 +35,49 @@ const closeTask = () => {
 
 const createTaskInputs = (taskDetails) => {
     
-    let taskDetailsNode = taskDetails
-    const title = document.createElement('textarea')
+    const title = elementCreator('textarea','taskTitle')
     title.setAttribute('placeholder','Title')
-    const details = document.createElement('textarea')
+    taskDetails.appendChild(title)
+
+    const details = elementCreator('textarea','details')
     details.setAttribute('placeholder','Details')
-    details.setAttribute('id', 'details')
-    const dateContainer = elementCreatorTest('div','dateContainer').elementNode
-    const dateTitle = elementCreatorTest('div','dateTitle','Date: ')
-    dateContainer.appendChild(dateTitle.elementNode)
-    const dateDue = elementCreatorTest('input','dateDue')
-    dateContainer.appendChild(dateDue.elementNode)
-    dateDue.elementNode.setAttribute('type','date')
-    const priorityContainer = elementCreatorTest('div','priorityContainer').elementNode
-    const priorityTitle = elementCreatorTest('div','priorityTitle','Priority: ')
-    priorityContainer.appendChild(priorityTitle.elementNode)
-    const priorityButtonsContainer = elementCreatorTest('div', 'priorityButtonsContainer').elementNode
+    taskDetails.appendChild(details)
+
+    const dateContainer = elementCreator('div','dateContainer')
+    taskDetails.appendChild(dateContainer)
+
+    const dateTitle = elementCreator('div','dateTitle','Date: ')
+    dateContainer.appendChild(dateTitle)
+
+    const dateDue = elementCreator('input','dateDue')
+    dateDue.setAttribute('type','date')
+    dateContainer.appendChild(dateDue)
+
+    const priorityContainer = elementCreator('div','priorityContainer')
+    taskDetails.appendChild(priorityContainer)
+
+    const priorityTitle = elementCreator('div','priorityTitle','Priority: ')
+    priorityContainer.appendChild(priorityTitle)
+
+    const priorityButtonsContainer = elementCreator('div', 'priorityButtonsContainer')
     
     const createPriorityButtons = (priorityButtonsContainer) => {
         const buttonOptions = ['Low','Medium','High']
         for(let i = 0; i < 3;i++) {
-            const priorityButton = elementCreatorTest('div',`priorityButton${i}`).elementNode
+            const priorityButton = elementCreator('div',`priorityButton${i}`)
             priorityButton.classList.add('button')
             priorityButton.textContent = buttonOptions[i]
             priorityButtonsContainer.appendChild(priorityButton)
         }
-        priorityContainer.appendChild(priorityButtonsContainer)
     }
     createPriorityButtons(priorityButtonsContainer)
+    taskDetails.appendChild(priorityButtonsContainer)
 
-    const nodeArray = [
-        title,details,dateContainer,
-        priorityContainer,priorityContainer
-    ]
-    
-    const createTaskInputsFunction = () => {
-        for(let i = 0;i < nodeArray.length;i++) {
-            taskDetailsNode.appendChild(nodeArray[i])
-        }
-        return taskDetailsNode
-    }
-    taskDetailsNode = createTaskInputsFunction()
-
-    return { taskDetailsNode }
+    return { taskDetails }
 
 }
 
-const elementCreatorTestTest = (type,id,textContent) => {
+const elementCreator = (type,id,textContent) => {
     
     const element = document.createElement(type)
     element.id = id
