12cd8ec99e9f42f7b45ca3bfbfb1cdcfd7789726
Mike Tagg <mdtagg91@gmail.com>
2022-12-20 15:02:39 -0800

    watch added to webpack config file
    -added watch:true to the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  watch:true,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
